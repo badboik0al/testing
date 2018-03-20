f76fbfe3925655a2b24d85744ab8e10919d98b77
badboik0al <37582261+badboik0al@users.noreply.github.com>
2018-03-20 09:34:19 -0300

    Add files via upload

---
+++ NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	
+	int max;
+	int min;
+	int guess;
+	
+	public int maxGuessesAllowed = 5;	
+	public Text text;
+	
+	void Start () {
+		StartGame ();
+	}
+	
+	
+	public void GuessLower (){
+					max = guess;
+					NextGuess ();			
+	}
+	
+	public void GuessHigher(){
+					min = guess;
+					NextGuess ();
+		}
+	
+	void StartGame () {
+				max = 1000;
+				min = 1;
+				NextGuess();
+		}
+	
+	void NextGuess () {
+	
+		guess = Random.Range (min, max+1);
+		text.text  = guess.ToString();
+		maxGuessesAllowed  = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed  <= 0){
+				Application.LoadLevel("Win");
+		}
+		
+	}
+	
+}
